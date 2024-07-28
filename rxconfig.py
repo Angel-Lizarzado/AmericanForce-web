00cc7c5a94df3166ab4cb419c3efc0054af2d848
Angel-Lizarzado <66540618+Angel-Lizarzado@users.noreply.github.com>
2024-07-28 01:31:52 -0400

    Add files via upload

--- rxconfig.py
+++ rxconfig.py
@@ -2,8 +2,9 @@ import reflex as rx
 
 config = rx.Config(
     app_name="ejercitoaf",
+    api_url="https://americanforce-web.up.railway.app",
     cors_allowed_origins=[
-        "http://localhost:3000"
+        "http://localhost:3000",
         "https://americanforce-web.vercel.app"
     ]
 )
